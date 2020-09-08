437808ca6898259de99386442d5ff8439e2dae7e
Cauê Alves <caue.mendes.rodrigues.alves@gmail.com>
2020-09-08 19:06:06 -0300

    Add files via upload

---
+++ Funcionarios/Diretor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    class Diretor : Funcionario
+    {
+        public Diretor(string cpf) : base(cpf)
+        {
+            Console.WriteLine("Criando DIRETOR...");
+        }
+        public override double GetBonificacao()
+        {
+            return Salario + base.GetBonificacao();
+        }
+    }
+}
---
+++ Funcionarios/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public class Funcionario
+    {
+        public static int TotalDeFuncionarios { get; private set; }
+        public string nome { get; set; }
+        public string CPF { get;}
+        public double Salario { get; set; }
+
+        public Funcionario(string cpf)
+        {
+            CPF = cpf;
+            TotalDeFuncionarios++;
+            Console.WriteLine("Criando FUNCIONÁRIO...");
+        }
+
+        public virtual double GetBonificacao() 
+        { 
+            return Salario * 0.10;
+        }
+    }
+}
