d9c4799a116e2fba83289dbbf42ae5cc12f0b63b
Cauê Alves <caue.mendes.rodrigues.alves@gmail.com>
2020-09-09 14:46:07 -0300

    Add files via upload

--- Funcionarios/Diretor.cs
+++ Funcionarios/Diretor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ByteBank.Sistemas;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,9 @@ using System.Threading.Tasks;
 
 namespace ByteBank.Funcionarios
 {
-    class Diretor : Funcionario
+    class Diretor : Autenticavel
     {
+
         public Diretor(string cpf) : base(cpf)
         {
             Console.WriteLine("Criando DIRETOR...");
--- Funcionarios/GerenteDeConta.cs
+++ Funcionarios/GerenteDeConta.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ByteBank.Sistemas;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ByteBank.Funcionarios
 {
-    class GerenteDeConta : Funcionario
+    class GerenteDeConta : Autenticavel
     {
         public GerenteDeConta(string cpf) : base(cpf)
         {
