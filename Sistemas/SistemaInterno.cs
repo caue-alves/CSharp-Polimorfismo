d7b890a44c13e476a129f580276d1464697d09c3
Cauê Alves <caue.mendes.rodrigues.alves@gmail.com>
2020-09-09 14:46:45 -0300

    Add files via upload

---
+++ Sistemas/SistemaInterno.cs
@@ -0,0 +1,28 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Sistemas
+{
+    class SistemaInterno
+    {
+        public bool Logar(Diretor oHome, string senha)
+        {
+            bool UsuarioAutenticado = oHome.Autenticar(senha);
+
+            if (UsuarioAutenticado)
+            {
+                Console.WriteLine("Você foi aceito!");
+                return true;
+            } else
+            {
+                Console.WriteLine("Errado, você não foi aceito");
+                return false;
+            }
+
+        }
+    }
+}
